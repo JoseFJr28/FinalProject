12ef8611d4316d9714f41f138a26fe36c4f3165b
Jose Fuentes Jr <jfuente7@terpmail.umd.edu>
2023-04-14 16:50:07 -0400

    The concentration skeleton

---
+++ concentrationskeleton.py
@@ -0,0 +1,12 @@
+"""
+The following is a game called Concentration. You are able to play by yourself
+or with the computer. The goal of the game is to name more object of the
+category options. There are 4 modes: easy, intermediate, hard, and impossible.
+"""
+
+class Concentration:
+    
+    
+    def __init__(self, game):
+        pass
+    
