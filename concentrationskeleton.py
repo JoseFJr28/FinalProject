133096b6f085e8d22221ccdc45bbd1c967f8f90e
Mohith Konduru <mohith.konduru@yahoo.com>
2023-05-01 09:27:07 -0400

    Added the leaderboard methods

--- concentrationskeleton.py
+++ concentrationskeleton.py
@@ -1,4 +1,7 @@
 import random
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
 """
 The following is a game called Concentration. You are able to play by yourself
 or with the computer. The goal of the game is to name more object of the
@@ -22,10 +25,43 @@ class Concentration:
     def npc_num(self, npc):
         return random(range(1,11))
     
-    
     'Adds player to the game'
     def add_players(self, player_dict):
         return player_dict[self.name]
     
-
-    
+    'Writes the players score onto a csv file'
+    def record_score(self, name, score, category):
+        with open('leaderboard.csv', mode = 'a') as filepath:
+            record = csv.writer(filepath, delimeter = ' ')
+            record.writerow([self.name, self.score, self.category])
+        
+    'Displays leaderboard'
+    def leaderboard(self):
+        data = pd.read("leaderboard.csv")
+        data = data["score" > 5]
+        data = data.groupby("name")
+        print(data)
+        
+    'Displays Score Distribution'
+    def score_distribution(self):
+        names = []
+        scores = []
+        categories = []
+        
+        with open(csv_filename, mode='r') as scores_file:
+            score_reader = csv.reader(scores_file)
+            for row in score_reader:
+                names.append(row[0])
+                scores.append(int(row[1]))
+                categories.append(row[2])
+                
+        fig, ax = plt.subplots()
+        ax.bar(names, scores)
+        ax.set_xlabel('Names')
+        ax.set_ylabel('Scores')
+        ax.set_title('Scores by Category')
+        ax.set_xticklabels(categories)
+        plt.show()
+        
+        
+        
