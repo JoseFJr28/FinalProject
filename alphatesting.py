0d3c43feee27db318261f53e4957c84a8545ddfe
Jose Fuentes Jr <jfuente7@terpmail.umd.edu>
2023-05-04 19:31:23 -0400

    Putting Two classes together

--- alphatesting.py
+++ alphatesting.py
@@ -1,16 +1,11 @@
 import random
-import sys
 from argparse import ArgumentParser
 from FinalCategoryListTesting2 import Categories
 from testingConcentration import Player
-import NPC
+#################################
 
 #################################
-import pandas as pd
-import matplotlib.pyplot as plt
-import csv
-import time 
-#################################
+
 
 """
 The following is a game called Concentration. You are able to play by yourself
@@ -21,8 +16,8 @@ category options. There are 4 modes: easy, intermediate, hard, and impossible.
 class Concentration:
     
     """This defines the game"""
-    def __init__(self, player_list):
-        self.player_listt = player_list
+    def __init__(self):
+        self.player_list = []
         
     def start_game(self, player_list):
         game_order = self.order(player_list)
@@ -32,7 +27,8 @@ class Concentration:
         category = input("Now choose a category? (Animals/Card Games/Training) ")
         game_state = self.npc_checker(game_order)
             
-            
+    def current_players(self):
+        return self.player_list
         
         
     def order(self, player_list):
@@ -82,6 +78,44 @@ class Concentration:
                 # continue
         self.reset_turns(player, player_list)
     
+    
+    def add_players(self, player_list):
+        """
+        Adds the current players in the game in a list
+        Args:
+            player_listt (list): list of players, mix of human and npc
+            player (Instance of Player): 
+        Side effects:
+            adds to the players list
+        """
+        return player_list.append(self.player)
+
+class NPC:
+    """
+    
+    Attributes:
+        mode (str): The difficulty of the the npc
+        npc_name(str): the name of the computer, default value of Sheldon
+        score(int): the score for the npc
+    """
+    def __init__(self, mode, npc_name= "Sheldon"):
+        """
+        Initilizes the mode the computer will be in
+        
+        Args:
+
+        """
+        self.mode = mode
+        self.npc_name = "Sheldon"
+        self.head = []
+    
+    
+    def __repr__(self):
+        """
+        This will print out hte NPC name and provide the current score 
+        """
+        return f"Current mode for NPC {self.npc_name} is {self.mode}"   
+    
     def handicap_npc(self, mode):
         """
         Will give a handicap to the NPC on how "smart" it is;
@@ -106,129 +140,93 @@ class Concentration:
         elif mode.lower() == 'hard'.lower():
             return random.randrange(30,50)
         elif mode.lower() == 'impossible'.lower():
-            return self.brain
+            return len(5)
         else:
-            return f"That mode is not available" 
+            return f"That mode is not available"        
+
     
-    def add_players(self, player_list, player):
-        """
-        Adds the current players in the game in a list
-        Args:
-            player_listt (list): list of players, mix of human and npc
-            player (Instance of Player): 
-        Side effects:
-            adds to the players list
-        """
-        return player_list.append(player)
     
-    def timer(self):
-        '''
-        This method controls the timer in the game 
-        it gives the players a certain amount of time to make a guess
-        '''
-        limit = 45
-        t1 = time.time()
-        while time.time()- t1 < limit:
-            if t1 <= limit: 
-                Player = 1
-            else: 
-                t1 > limit
-                elimination = player_dict.pop(Player)
-                return f"You hestitated, {elimination} is eliminatined"
-            
-    def player_words(self, player_words):
-        '''
-        This functions holds player words and keeps track of the guess made
-        by all players 
-        '''
+    def npc_reader(self, category,mode):
+        """
+        Will read the file and from there will see what difficulty of the npc
+        and return a list of the words the computer will know. The words will be
+        chosen at random
         
-        self.player_words = player_words
-        player_words = {Player:[]} 
-    
-    
-    # def timer(self):
-    #     start_time = time.time()
-    #     response = time.time()
-    #     time_lasp = start_time - response
-    #     return time_lasp
-    #     if time_lasp > avg_time():
-    #         return f"You hestitated, {winner} wins"
-    #     else():
-            # pass
-    
+        Ars:
+           mode(str): difficulty of the npc
+            categorty(dict): This chosen category that will have the list of 
+            words in that category
+        Returns:
+            A list of random words from the categories list of words
+        """
+        #This allows to go through how many answers we have in total
+        length = 0
+        for x in Categories:
+            if x.lower() == category.lower():
+                length += len(Categories.get(category))
+        
+        #This allows access the available words
+        #Making sure that he doesn't go over the required amount of words to know
+            brain_size = self.handicap_npc(mode)
+            if len(self.head) != brain_size:
+                newcount = 0
+                final = []
+    #Now as we go through the restricted number of words to know    
+                while newcount < brain_size:
+                    #we pick a random number from the size of the available words
+                    pick = random.randrange(length)
+    #Access the words the itself
+                    for x in Categories.get(category):
+    #We make sure the random number is the key we want and its not in the list and 
+    #the word is not in the list( to prevent repeats) we add it
+                        if x == pick and x not in final:
+                            final.append(Categories[category][x])
+                        else:
+                            continue
+                #prevents the infinite loop            
+                    newcount += 1
+                return final
+            
     
 def main():
-    answer = input("Welcome To Concentration 64!!!! Ready to begin your journey of fun (Yes/No)?")
+    new_game = Concentration()
+    answer = input("Welcome To Concentration 64!!!! Ready to begin your journey of fun (Yes/No)? ")
     if answer.lower() == 'Yes'.lower():
         print("Great! Lets begin!")
-        print("""NOTE: If there is only 1 player in the game you will automatically have a computer player to go against.
-              If you want to include a computer player just type npc""")
-        player_count = int(input("How many players will be there? (1-4)"))
+        print("""NOTE: If there is only 1 player in the game you will automatically
+        have a computer player to go against. If you want to include a computer player just type npc""")
+        player_count = int(input("How many players will be there? (1-4) "))
         if player_count == 1:
             player_name = input("What is your name? ")
             player1 = Player(player_name)
-            npc_player = NPC(mode)
-            Concentration.players_list.append(player1)
-            Concentration.players_list.append(npc_player)
+            difficulty = input("what mode would you prefer to play? (Easy/Medium/Hard/Impossible) ")
+            npc_player = NPC(difficulty)
+            new_game.add_players(player1)
+            new_game.add_players(npc_player)
             print("Let the games begin")
-            start_game(Concentration.players_list)
-        elif player_count > 1:
+            new_game.start_game(new_game.players_list)
+        elif player_count > 1 and player_count <= 4:
             start = 0
             while start < player_count:
                 player = input("What's your name? ")
                 if player == 'npc':
-                    new_npc = NPC(mode)
-                    Concentration.players_list.append(new_npc)
+                    difficulty = input("what mode would you prefer to play? (Easy/Medium/Hard/Impossible) ")
+                    npc_player = NPC(difficulty)
+                    new_game.add_players(npc_player)
                 else:
-                    Concentration.players_list.append(player)
+                    new_game.add_players(player)
                 start += 1
             print("Let the games begin")
-            start_game(Concentration.players_list)
+            new_game.start_game(new_game.current_players)
         elif player_count > 4:
             print("Sorry! That is too many players. The game will crash now")
     elif answer.lower() == 'No'.lower():
         print("Okay! We tried :( Just know I could've beaten you with half my power!)")
     else:
-        print("""So you don't know wha to say? Well I will say it for you. Thank You come again! \n
+        print("""So you don't know wha to say? Well I will say it for you. Thank You come again!
               Don't forget to leave a 5 star rating on Yelp""")
 
 
 if __name__ == '__main__':
     main()
 
-#this is where the leaderboard starts where Mo added
-#Make sure to mention any pip installs
-    'Writes the players score onto a csv file'
-    def record_score(self, name, score, category):
-        with open('leaderboard.csv', mode = 'a', encoding='utf-8') as filepath:
-            record = csv.writer('leaderboard.csv', dialect='excel', delimeter = ' ')
-            record.writerow([self.name, self.score, self.category])
-        
-    'Displays leaderboard'
-    def leaderboard(self):
-        data = pd.read("leaderboard.csv")
-        data = data["score" > 5]
-        data = data.groupby("name")
-        print(data)
-        
-    'Displays Score Distribution'
-    def score_distribution(self):
-        names = []
-        scores = []
-        categories = []
-        
-        with open(csv_filename, mode='r') as scores_file:
-            score_reader = csv.reader(scores_file)
-            for row in score_reader:
-                names.append(row[0])
-                scores.append(int(row[1]))
-                categories.append(row[2])
-                
-        fig, ax = plt.subplots()
-        ax.bar(names, scores)
-        ax.set_xlabel('Names')
-        ax.set_ylabel('Scores')
-        ax.set_title('Scores by Category')
-        ax.set_xticklabels(categories)
-        plt.show()
-    
